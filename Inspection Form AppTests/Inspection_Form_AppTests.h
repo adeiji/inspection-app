7b409f2a373e43035930037ba5009a1d473d7ae5
Developer <Developer@Jeffrey-Najarros-MacBook-Pro.local>
2012-03-09 14:36:56 -0800

    Initial Commit

---
+++ Inspection Form AppTests/Inspection_Form_AppTests.h
@@ -0,0 +1,13 @@
+//
+//  Inspection_Form_AppTests.h
+//  Inspection Form AppTests
+//
+//  Created by Developer on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Inspection_Form_AppTests : SenTestCase
+
+@end
---
+++ Inspection Form AppTests/Inspection_Form_AppTests.m
@@ -0,0 +1,32 @@
+//
+//  Inspection_Form_AppTests.m
+//  Inspection Form AppTests
+//
+//  Created by Developer on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Inspection_Form_AppTests.h"
+
+@implementation Inspection_Form_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Inspection Form AppTests");
+}
+
+@end
